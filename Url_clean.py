36a6ebec28473c33d4da7e12ab50d55df28a197c
Kiyokata <138622886+kiyokata7@users.noreply.github.com>
2023-07-05 02:13:08 +0600

    Create Url_clean.py

---
+++ Url_clean.py
@@ -0,0 +1,33 @@
+lastDomain = set()
+
+print("......Working")
+
+with open('url.txt', 'r', errors='ignore') as file:
+    urls = file.readlines()
+    total = len(urls)
+    valid_urls = []
+
+    print(f"Total Links: {total}")
+
+    for url in urls:
+        if '?' in url and '=' in url and url.startswith('http') and ".kr" not in url:
+            try:
+                current_domain = url.split('/')[2].strip()
+
+                if current_domain in lastDomain:
+                    pass
+                else:
+                    lastDomain.add(current_domain)
+
+                    with open("valid.txt", 'a') as output_file:
+                        output_file.write(url)
+
+                    valid_urls.append(url)
+
+            except IndexError:
+                continue
+
+    total_valid = len(valid_urls)
+    print(f"Valid Links: {total_valid}")
+
+input("Finished......")
